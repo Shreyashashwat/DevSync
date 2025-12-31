48035a8cad992fb797ac48490fc5cfa4dae895c6
Shreyashashwat <shashwatshreya251@gmail>
2025-12-31 19:02:40 +0530

    Deploy to Railway

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ console.log(">>> WORKING DIRECTORY:", process.cwd());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"]
+  origin: ["http://localhost:3000","http://localhost:5173", "http://localhost:5174"]
 }));
 
 
